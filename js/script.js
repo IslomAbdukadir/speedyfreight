a5793138a5ee9723f1f3ca238afced4cde7d0603
islomabduqodir@gmail.com <islomabduqodir@gmail.com>
2024-05-22 00:27:39 +0500

    new

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 // burger menu
 
-let list = document.querySelector(".header-list");
+function sendRequest(nameSelector,experienceSelector,emailSelector, phoneSelector) {
+    let name = document.querySelector(nameSelector).value;
+    let experience = document.querySelector(experienceSelector).value;
+    let email = document.querySelector(emailSelector).value;
+    let phone = document.querySelector(phoneSelector).value;
+    let requestText = encodeURI(`<b>Заявка</b>\nName: ${name}\nExpirence: ${experience}\nEmail: ${email}\nPhone: ${phone}`)
+    fetch(`https://api.telegram.org/bot6845851859:AAEnvJxZm_ytyAzJIPDd5Y9VpvlqD0cbmNI/sendMessage?text=${requestText}&chat_id=-4264174768&parse_mode=HTML`)
+}
 
-$(".menu").click(function () { 
-    list.style.display = 'flex';   
-    $(".header-link-exit").click(function () { 
-        $(".header-list").hide();    
-    }); 
-});
-
-// burger menu
+document.querySelector('.form-btn').addEventListener('click', (value) => {
+    value.preventDefault()
+    sendRequest(`.header-form input[name="name"]`, `.header-form input[name="experience"]`, `.header-form input[name="email"]`, `.header-form input[name="phone"]`)
+})
